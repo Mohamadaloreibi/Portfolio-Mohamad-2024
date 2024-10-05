6408f81dd202714665d4e458663240b827f04a68
Mohamed Al-oreibi <Moh2567@outlook.com>
2024-10-06 00:05:49 +0200

    new file vercel.json and change in vite.config.js

--- Documents/Portfolio-2024/portfolio-2024/vite.config.js
+++ Documents/Portfolio-2024/portfolio-2024/vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // eller sätt basen för var projektet distribueras om inte i root
 });
